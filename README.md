d3e7e4889531590be57361a972de2d91d13846e3
André Mattos <carlosandrematte@gmail.com>
2024-03-04 10:51:32 -0300

    Added u-net model

--- README.md
+++ README.md
@@ -18,3 +18,4 @@ You can check each model's summaries by running the python file. All model infor
 | Karen Simonyan    | 2015 | [VGG-19](https://github.com/andrematte/deep-learning-architectures/blob/master/models/vgg19.py)        | [Paper Link](http://arxiv.org/abs/1409.1556)                      |
 | Christian Szegedy | 2015 | [GoogLeNet](https://github.com/andrematte/deep-learning-architectures/blob/master/models/googlenet.py) | [Paper Link](http://ieeexplore.ieee.org/document/7298594/)        |
 | Kaiming He        | 2015 | [ResNet-50](https://github.com/andrematte/deep-learning-architectures/blob/master/models/resnet50.py)  | [Paper Link](https://arxiv.org/pdf/1512.03385.pdf)                |
+| Olaf Ronneberger  | 2015 | [U-Net](https://github.com/andrematte/deep-learning-architectures/blob/master/models/unet.py)  | [Paper Link](https://arxiv.org/abs/1505.04597)                |
---
+++ models/unet.py
@@ -0,0 +1,108 @@
+# U-Net Architecture
+# Author: Olaf Ronneberger, 2015
+# Adapted from: Sreenivas Bhattiprolu, Python for Microscopists
+# Paper link: https://arxiv.org/abs/1505.04597
+
+from tensorflow.keras.layers import (
+    concatenate,
+    Conv2D,
+    Conv2DTranspose,
+    Dropout,
+    Input,
+    Lambda,
+    MaxPooling2D,
+)
+
+from tensorflow.keras import Model
+
+
+def unet(input_width=256, input_height=256, input_channels=3, metrics=["accuracy"]):
+
+    # Building the Model
+    inputs = Input((input_height, input_width, input_channels))
+    s = Lambda(lambda x: x / 255)(inputs)
+
+    # Contraction path
+    c1 = Conv2D(16, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(s)
+    c1 = Dropout(0.1)(c1)
+    c1 = Conv2D(16, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+
+    c2 = Conv2D(32, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Dropout(0.1)(c2)
+    c2 = Conv2D(32, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+
+    c3 = Conv2D(64, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Dropout(0.2)(c3)
+    c3 = Conv2D(64, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+
+    c4 = Conv2D(128, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Dropout(0.2)(c4)
+    c4 = Conv2D(128, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+
+    c5 = Conv2D(256, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Dropout(0.3)(c5)
+    c5 = Conv2D(256, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(c5)
+
+    # Expansive path
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(128, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Dropout(0.2)(c6)
+    c6 = Conv2D(128, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(c6)
+
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(64, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Dropout(0.2)(c7)
+    c7 = Conv2D(64, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(c7)
+
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(32, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Dropout(0.1)(c8)
+    c8 = Conv2D(32, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(c8)
+
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(16, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Dropout(0.1)(c9)
+    c9 = Conv2D(16, (3, 3), activation='relu',
+                kernel_initializer='he_normal', padding='same')(c9)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer="adam",
+                  loss="binary_crossentropy", metrics=metrics)
+
+    return model
+
+
+def main():
+    model = unet()
+    print(model.summary())
+
+
+if __name__ == "__main__":
+    main()
